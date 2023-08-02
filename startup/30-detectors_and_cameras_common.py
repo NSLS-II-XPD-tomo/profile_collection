36df11153d84594c10805032b399ec598ddcbef9
Mehmet Topsakal <metokal@gmail.com>
2023-08-02 13:39:13 -0400

    HDF5 writing is fully implemented and tested now

--- startup/30-detectors_and_cameras_common.py
+++ startup/30-detectors_and_cameras_common.py
@@ -6,7 +6,6 @@ print(f"Loading {__file__}")
 # and
 # https://github.com/NSLS-II-XPD/profile_collection
 
-
 import ophyd
 from ophyd.areadetector import (
     AreaDetector,
@@ -27,6 +26,7 @@ from ophyd.areadetector.filestore_mixins import (
     FileStoreIterativeWrite,
     FileStoreTIFF,
     FileStoreBase,
+    FileStorePluginBase,
 )
 
 
@@ -39,6 +39,12 @@ from ophyd.areadetector import EpicsSignalWithRBV as SignalWithRBV
 
 from ophyd.device import BlueskyInterface
 from ophyd.device import DeviceStatus
+
+
+from ophyd.areadetector.trigger_mixins import SingleTrigger
+
+
+from collections import OrderedDict
 
 
 class XPDCamBase(CamBase):
@@ -61,28 +67,12 @@ class XPDCamBase(CamBase):
                 cpt.ensure_nonblocking()
 
 
+# TIFF stuff
 class XPDTIFFPlugin(TIFFPlugin, FileStoreTIFFSquashing, FileStoreIterativeWrite):
     pass
 
 
-class FileStoreHDF5IterativeWrite(FileStoreTIFFSquashing):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.filestore_spec = "AD_HDF5"  # spec name stored in resource doc
-        self.stage_sigs.update(
-            [
-                ("file_template", "%s%s_%6.6d.h5"),
-            ]
-        )
-
-
-class XPDHDF5Plugin(
-    HDF5Plugin_V33, FileStoreHDF5IterativeWrite, FileStoreIterativeWrite
-):
-    pass
-
-
-class XPDAreaDetector(AreaDetector):
+class XPDAreaDetector_tiff(AreaDetector):
     cam = Component(
         XPDCamBase,
         "cam1:",
@@ -97,24 +87,15 @@ class XPDAreaDetector(AreaDetector):
 
     image = Component(ImagePlugin, "image1:")
 
-    # tiff = Component(
-    #     XPDTIFFPlugin,
-    #     "TIFF1:",
-    #     write_path_template="/a/b/c/",
-    #     read_path_template="/a/b/c",
-    #     cam_name="cam",
-    #     proc_name="proc",
-    #     read_attrs=[],
-    # )
-
-    hdf5 = Component(
-        XPDHDF5Plugin,
-        "HDF1:",
+    tiff = Component(
+        XPDTIFFPlugin,
+        "TIFF1:",
         write_path_template="/a/b/c/",
         read_path_template="/a/b/c",
         cam_name="cam",
         proc_name="proc",
         read_attrs=[],
+        root="/nsls2/data/xpd-new/legacy/raw/xpdd",
     )
 
     proc = Component(ProcessPlugin, "Proc1:")
@@ -140,7 +121,7 @@ class XPDAreaDetector(AreaDetector):
         self.proc.stage_sigs.update(
             [
                 (self.proc.filter_type, "RecursiveAve"),
-                (self.proc.data_type_out, "Float32"),
+                (self.proc.data_type_out, "UInt16"),
             ]
         )
 
@@ -167,7 +148,6 @@ class ContinuousAcquisitionTrigger(BlueskyInterface):
         self.cam.stage_sigs["acquire"] = 1
 
         self._plugin.stage_sigs[self._plugin.file_write_mode] = "Capture"
-        # self._plugin.stage_sigs[self._plugin.file_write_mode] = "Stream"
         self._image_name = image_name
         self._status = None
         self._num_captured_signal = self._plugin.num_captured
@@ -207,8 +187,7 @@ class ContinuousAcquisitionTrigger(BlueskyInterface):
             # This is run on a thread, so exceptions might pass silently.
             # Print and reraise so they are at least noticed.
             try:
-                # self.hdf5.write_file.put(1)
-                self.hdf5.write_file.put(1)
+                self.tiff.write_file.put(1)
             except Exception as e:
                 print(e)
                 raise
@@ -219,7 +198,200 @@ class ContinuousAcquisitionTrigger(BlueskyInterface):
             self._save_started = False
 
 
-class XPDContinuous(ContinuousAcquisitionTrigger, XPDAreaDetector):
+class XPDAreaDetector_tiff(ContinuousAcquisitionTrigger, XPDAreaDetector_tiff):
+    def make_data_key(self):
+        source = "PV:{}".format(self.prefix)
+        # This shape is expected to match arr.shape for the array.
+        shape = (
+            self.number_of_sets.get(),
+            self.cam.array_size.array_size_y.get(),
+            self.cam.array_size.array_size_x.get(),
+        )
+        return dict(shape=shape, source=source, dtype="array", external="FILESTORE:")
+
+    pass
+
+
+# HDF5 stuff
+class FileStoreHDF5Squashing(FileStorePluginBase):
+    r"""Write out 'squashed' HDF5
+
+    .. note::
+
+       See :class:`FileStoreBase` for the rest of the required parametrs
+
+    This mixin will also configure the ``cam`` and ``proc`` plugins
+    on the parent.
+
+    This is useful to work around the dynamic range of detectors
+    and minimizing disk spaced used by synthetically increasing
+    the exposure time of the saved images.
+
+    Parameters
+    ----------
+    images_per_set_name, number_of_sets_name : str, optional
+        The names of the signals on the parent to get the
+        images_pre_set and number_of_sets from.
+
+        The total number of frames extracted from the camera will be
+        :math:`number\_of\_sets * images\_per\_set` and result in
+        ``number_of_sets`` frames in the HDF5 file each of which is the average of
+        ``images_per_set`` frames from the detector.
+
+        Defaults to ``'images_per_set'`` and ``'number_of_sets'``
+
+    cam_name : str, optional
+        The name of the :class:`~ophyd.areadetector.cam.CamBase`
+        instance on the parent.
+
+        Defaults to ``'cam'``
+
+    proc_name : str, optional
+        The name of the
+        :class:`~ophyd.areadetector.plugins.ProcessPlugin` instance on
+        the parent.
+
+        Defaults to ``'proc1'``
+
+    Notes
+    -----
+
+    This class in cooperative and expected to particpate in multiple
+    inheritance, all ``*args`` and extra ``**kwargs`` are passed up the
+    MRO chain.
+
+    """
+
+    def __init__(
+        self,
+        *args,
+        images_per_set_name="images_per_set",
+        number_of_sets_name="number_of_sets",
+        cam_name="cam",
+        proc_name="proc1",
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.filestore_spec = "AD_HDF5"  # spec name stored in resource doc
+        self._ips_name = images_per_set_name
+        self._num_sets_name = number_of_sets_name
+        self._cam_name = cam_name
+        self._proc_name = proc_name
+        cam = getattr(self.parent, self._cam_name)
+        proc = getattr(self.parent, self._proc_name)
+        self.stage_sigs.update(
+            [
+                ("file_template", "%s%s_%6.6d.h5"),
+                ("file_write_mode", "Stream"),
+                ("capture", 1),
+                (proc.nd_array_port, cam.port_name.get()),
+                (proc.reset_filter, 1),
+                (proc.enable_filter, 1),
+                (proc.filter_type, "RecursiveAve"),
+                (proc.auto_reset_filter, 1),
+                (proc.filter_callbacks, 1),
+                ("nd_array_port", proc.port_name.get()),
+            ]
+        )
+
+    def get_frames_per_point(self):
+        return getattr(self.parent, self._num_sets_name).get()
+
+    def stage(self):
+        # print(f"Before staging: {self._fn = }\n{self._fp = }")
+
+        cam = getattr(self.parent, self._cam_name)
+        proc = getattr(self.parent, self._proc_name)
+        images_per_set = getattr(self.parent, self._ips_name).get()
+        num_sets = getattr(self.parent, self._num_sets_name).get()
+
+        self.stage_sigs.update(
+            [
+                (proc.num_filter, images_per_set),
+                (cam.num_images, images_per_set * num_sets),
+            ]
+        )
+        super().stage()
+        resource_kwargs = {
+            "frame_per_point": self.get_frames_per_point(),
+        }
+        self._generate_resource(resource_kwargs)
+        # print(f"After staging: {self._fn = }\n{self._fp = }")
+
+
+class XPDHDF5Plugin(HDF5Plugin_V33, FileStoreHDF5Squashing, FileStoreIterativeWrite):
+    pass
+
+
+class XPDAreaDetector_hdf5(AreaDetector):
+    cam = Component(
+        XPDCamBase,
+        "cam1:",
+        read_attrs=[],
+        configuration_attrs=[
+            "image_mode",
+            "trigger_mode",
+            "acquire_time",
+            "acquire_period",
+        ],
+    )
+
+    image = Component(ImagePlugin, "image1:")
+
+    # tiff = Component(
+    #     XPDTIFFPlugin,
+    #     "TIFF1:",
+    #     write_path_template="/a/b/c/",
+    #     read_path_template="/a/b/c",
+    #     cam_name="cam",
+    #     proc_name="proc",
+    #     read_attrs=[],
+    # )
+
+    hdf5 = Component(
+        XPDHDF5Plugin,
+        "HDF1:",
+        write_path_template="/a/b/c/",
+        read_path_template="/a/b/c",
+        cam_name="cam",
+        proc_name="proc",
+        read_attrs=[],
+        root="/nsls2/data/xpd-new/legacy/raw/xpdd",
+    )
+
+    proc = Component(ProcessPlugin, "Proc1:")
+
+    # These attributes together replace `num_images`. They control
+    # summing images before they are stored by the detector (a.k.a. "tiff
+    # squashing").
+    images_per_set = Component(Signal, value=1, add_prefix=())
+    number_of_sets = Component(Signal, value=1, add_prefix=())
+
+    stats1 = Component(StatsPluginV33, "Stats1:", kind="hinted")
+    stats2 = Component(StatsPluginV33, "Stats2:")
+    stats3 = Component(StatsPluginV33, "Stats3:")
+    stats4 = Component(StatsPluginV33, "Stats4:")
+
+    roi1 = Component(ROIPlugin, "ROI1:")
+    roi2 = Component(ROIPlugin, "ROI2:")
+    roi3 = Component(ROIPlugin, "ROI3:")
+    roi4 = Component(ROIPlugin, "ROI4:")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.proc.stage_sigs.update(
+            [
+                (self.proc.filter_type, "RecursiveAve"),
+                (self.proc.data_type_out, "UInt16"),
+            ]
+        )
+
+
+class XPDAreaDetector_hdf5(SingleTrigger, XPDAreaDetector_hdf5):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.cam.stage_sigs["image_mode"] = "Single"
+
     def make_data_key(self):
         source = "PV:{}".format(self.prefix)
         # This shape is expected to match arr.shape for the array.
@@ -234,7 +406,6 @@ class XPDContinuous(ContinuousAcquisitionTrigger, XPDAreaDetector):
 
 
 def warmup_det(det):
-
     """
     Adapted from https://github.com/bluesky/ophyd/blob/7612b2c9de9d5bc16cf28eea79ba5c12553f3cc2/ophyd/areadetector/plugins.py#L966
     For hdf5 plugin "priming". ophyd version on;y supports Internal trigger mode.
@@ -289,9 +460,9 @@ def warmup_det(det):
             [
                 (det.cam.array_callbacks, 1),
                 (det.cam.image_mode, "Single"),
-                (det.cam.trigger_mode, "Off"),
+                # (det.cam.trigger_mode, "Off"),
                 (det.cam.acquire_time, 1),
-                (det.cam.acquire_period, 1),
+                # (det.cam.acquire_period, 1),
                 (det.cam.acquire, 1),
             ]
         )
--- startup/90-functions.py
+++ startup/90-functions.py
@@ -1,108 +1,9 @@
 print(f"Loading {__file__}")
 
 
-# def configure_det(
-#     det, acq_time=None, acq_period=None, exposure=None, num_exposures=1, sleep=0.2
-# ):
-#     if det.cam.acquire.get() == 0:
-#         yield from bps.abs_set(det.cam.acquire, 1, wait=True)
-#         time.sleep(0.1)
-
-#     if acq_time is None:
-#         acq_time = det.cam.acquire_time.get()
-
-#     if exposure is None:
-#         exposure = acq_time_rbv * 10
-
-#     if det.name == "xs3":
-#         yield from bps.abs_set(det.cam.acquire_time, max(acq_time, 0.001), wait=True)
-#         acq_time_rbv = det.cam.acquire_time.get()
-#         num_frames = np.ceil(exposure / acq_time_rbv)
-#         yield from bps.abs_set(det.cam.num_images, num_frames, wait=True)
-#         yield from bps.abs_set(det.proc_plugin.num_filter, num_frames, wait=True)
-#         print(
-#             ">>>%s is configured as:\n acq_time = %.3fsec; exposure = %.3fsec (num frames = %.2f) "
-#             % (det.name, acq_time_rbv, exposure, num_frames)
-#         )
-#         return
-
-#     if det.name == "dexela":
-#         yield from bps.abs_set(det.cam.acquire_time, max(acq_time, 0.0625), wait=True)
-#         acq_time_rbv = det.cam.acquire_time.get()
-#     else:
-#         yield from bps.abs_set(det.cam.acquire_time, acq_time, wait=True)
-#         acq_time_rbv = det.cam.acquire_time.get()
-
-#     if det.name == "dexela":
-#         yield from bps.abs_set(det.cam.acquire_period, acq_time_rbv + 0.005, wait=True)
-#         acq_period_rbv = det.cam.acquire_period.get()
-#     else:
-#         if acq_period is None:
-#             if det.name == "blackfly":
-#                 yield from bps.abs_set(det.cam.acquire_period, 0.1, wait=False)
-#                 time.sleep(0.2)
-#             else:
-#                 if det.name == "alliedvision":
-#                     pass
-#                 elif det.name == "prosilica":
-#                     yield from bps.abs_set(
-#                         det.cam.acquire_period, max([0.115,acq_time_rbv]), wait=False)
-#                 else:
-#                     yield from bps.abs_set(
-#                         det.cam.acquire_period, acq_time_rbv, wait=False)
-#             acq_period_rbv = det.cam.acquire_period.get()
-#         else:
-#             if det.name == "blackfly":
-#                 yield from bps.abs_set(
-#                     det.cam.acquire_period, min(1, acq_period), wait=False)
-#                 time.sleep(0.2)
-#                 acq_period_rbv = det.cam.acquire_period.get()
-#             elif det.name == "alliedvision":
-#                 acq_period_rbv = det.cam.acquire_period.get()
-#             else:
-#                 yield from bps.abs_set(det.cam.acquire_period, acq_period, wait=False)
-#                 acq_period_rbv = det.cam.acquire_period.get()
-
-#     num_frames = np.ceil(exposure / acq_time_rbv)
-#     yield from bps.abs_set(det.images_per_set, num_frames, wait=True)
-#     yield from bps.abs_set(det.number_of_sets, num_exposures, wait=True)
-
-#     if det.name == "emergent":
-#         print(
-#             ">>>%s is configured as:\n acq_time = %.3fmsec;  acq_period = %.3fmsec; exposure = %.3fmsec \
-#     (num frames = %.2f); num_exposures = %d"
-#             % (
-#                 det.name,
-#                 acq_time_rbv,
-#                 acq_period_rbv,
-#                 exposure,
-#                 num_frames,
-#                 num_exposures,
-#             )
-#         )
-#     else:
-#         print(
-#             ">>>%s is configured as:\n acq_time = %.3fsec;  acq_period = %.3fsec; exposure = %.3fsec \
-#     (num frames = %.2f); num_exposures = %d"
-#             % (
-#                 det.name,
-#                 acq_time_rbv,
-#                 acq_period_rbv,
-#                 exposure,
-#                 num_frames,
-#                 num_exposures,
-#             )
-#         )
-
-#     time.sleep(sleep)
-
-#     return
-
-
 def configure_det(
     det, acq_time=None, acq_period=None, exposure=None, num_exposures=None, sleep=None
 ):
-
     # get current values for acq_time, acq_period, image_mode, trigger_mode and acquire status
 
     # acq_time
@@ -198,8 +99,8 @@ def configure_det(
                 wait=True,
             )
 
-        if image_mode_ != 2:
-            yield from bps.abs_set(det.cam.image_mode, 2, wait=True)
+        # if image_mode_ != 2:
+        #     yield from bps.abs_set(det.cam.image_mode, 2, wait=True)
         if exposure is None:
             exposure = 4 * det.cam.acquire_time.get()
         else:
@@ -231,8 +132,8 @@ def configure_det(
                 wait=True,
             )
 
-        if image_mode_ != 2:
-            yield from bps.abs_set(det.cam.image_mode, 2, wait=True)
+        # if image_mode_ != 2:
+        #     yield from bps.abs_set(det.cam.image_mode, 2, wait=True)
         if exposure is None:
             exposure = 4 * det.cam.acquire_time.get()
         else:
@@ -283,68 +184,3 @@ def configure_det(
                 num_exposures,
             )
         )
-
-
-def oscillator(
-    motor: ophyd.Device,
-    oscillate_center: float,
-    oscillate_range: float,
-    oscillate_time: float,
-):
-    """
-    Move motor from left to right (oscillate) for oscillate_time seconds.
-    oscillate_center: center of motor position.
-    oscillate_range: range of oscillation.
-    oscillate_time: oscillate time in seconds.
-    """
-
-    t0 = time.time()
-
-    while time.time() - t0 < oscillate_time:
-        yield from bps.mv(motor, oscillate_center + oscillate_range)
-        yield from bps.mv(motor, oscillate_center - oscillate_range)
-
-
-def read_tiff_as_xarray(
-    tiffpath, figsize=(6, 6), robust=True, plot=False, cbar=False, mode=None
-):
-    """
-    Reads a tiff file as xarray
-    """
-
-    if mode == "prosilica":
-        img = fabio.open(tiffpath).data
-    else:
-        img = tifffile.imread(tiffpath).astype("float32")
-
-    da = xr.DataArray(
-        data=img,
-        coords=[np.arange(img.shape[0]), np.arange(img.shape[1])],
-        dims=["pixel_y", "pixel_x"],
-    )
-
-    if plot:
-        fig = plt.figure(figsize=figsize)
-
-        ax = fig.add_subplot(1, 1, 1)
-
-        if not cbar:
-            xp = da.plot.imshow(
-                ax=ax, robust=robust, yincrease=False, cmap="Greys_r", add_colorbar=cbar
-            )
-        else:
-            xp = da.plot.imshow(
-                ax=ax,
-                robust=robust,
-                yincrease=False,
-                cmap="Greys_r",
-                cbar_kwargs=dict(
-                    orientation="vertical", pad=0.07, shrink=0.4, label="Intensity"
-                ),
-            )
-
-        xp.axes.set_aspect("equal", "box")
-
-        plt.tight_layout()
-
-    return da
